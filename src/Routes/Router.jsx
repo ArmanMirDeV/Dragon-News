d0a60cbc45e93b692ee1fa7196dfa127844aba1e
Arman Mir <mirarman8583@gmail.com>
2025-10-19 06:36:10 +0600

    Design Grid Layouts and fetch categories using use hook

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,27 +1,35 @@
 import { createBrowserRouter } from "react-router";
 import HomeLayout from "../Layouts/HomeLayout";
+import Home from "../Pages/Home";
+import CategoryNews from "../Pages/CategoryNews";
 
-const router = createBrowserRouter(
-    [
-        {
-            path: '/',
-            element: <HomeLayout></HomeLayout>
-        },
-        {
-            path: '/auth',
-            element: <h2>Auth Layout</h2>
-        },
-        {
-            path: '/news',
-            element: <h2>News Layout</h2>
-        },
-        {
-            path: '/*',
-            element: <h2>Error 404</h2>
-        },
-    ]
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout></HomeLayout>,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/category/:id",
+        element: <CategoryNews />
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <h2>Auth Layout</h2>,
+  },
+  {
+    path: "/news",
+    element: <h2>News Layout</h2>,
+  },
+  {
+    path: "/*",
+    element: <h2>Error 404</h2>,
+  },
+]);
 
 export default router;
-
-
